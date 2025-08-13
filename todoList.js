9a8a54ecc99277dd8cdcb7cc9ac1db67808435e8
unwiredo <kentu.dsk@gmail.com>
2025-08-14 00:57:55 +0100

    Not completed version

---
+++ todoList.js
@@ -0,0 +1,78 @@
+document.querySelector('.js-addTodo')
+    .addEventListener('click', () => {
+        todoListInstance.addTodo();
+    });
+
+document.querySelector('.js-todo-input')
+    .addEventListener('keydown', (event) => {
+        if(event.key === 'Enter'){
+            todoListInstance.addTodo();
+        }
+    })
+
+class TodoList {
+    todoList = JSON.parse(localStorage.getItem('todos')) || [];
+    renderTodoList(){
+        let todoListHTML = '';
+        this.todoList.forEach((todo, index) =>{
+            const html = `<div class="todoParagraph">
+
+            <button class="js-done doneButton ${todo.done? 'isChecked': ''}"></button>
+            <p class="${todo.done?'textChecked' : ''}">${todo.name}</p>
+            <button class="js-delete-button delete-button"><img src="./images/trash.png" alt="Delete" class="trashPNG"></button>
+
+            </div>`;
+            todoListHTML += html;
+        });
+
+        document.querySelector('.js-todos')
+            .innerHTML = todoListHTML;
+
+        document.querySelectorAll('.js-delete-button').forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                this.removeTodo(index);
+            });
+        });
+
+        document.querySelectorAll('.js-done')
+            .forEach((doneButton, index) => {
+                doneButton.addEventListener('click', () => {
+                    this.todoList[index].done = !this.todoList[index].done;
+                    this.saveTodo();
+                    this.renderTodoList();
+                })
+            });
+    }
+
+    addTodo(){
+        const inputElement = document.querySelector('.js-todo-input');
+        const name = inputElement.value;
+        if(name === ''){
+            this.addTodoMessage();
+        }else{
+            this.todoList.push({
+            name: name,
+            });
+        }
+        inputElement.value = '';
+        this.renderTodoList();
+        this.saveTodo();
+    }
+
+    removeTodo(index){
+        this.todoList.splice(index, 1);
+        this.saveTodo();
+        this.renderTodoList();
+    }
+
+    saveTodo(){
+        localStorage.setItem('todos', JSON.stringify(this.todoList));
+    }
+
+    addTodoMessage(){
+        
+    }
+}
+
+const todoListInstance = new TodoList();
+const date = new Date();
